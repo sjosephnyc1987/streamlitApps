5de3fe726259586f426bfe786c346fba202e2484
Stephen Joseph <66809337+sjosephnyc1987@users.noreply.github.com>
2022-05-13 14:54:04 -0500

    Add files via upload

--- myStApp.py
+++ myStApp.py
@@ -7,6 +7,14 @@ Created on Thu May 12 16:24:48 2022
 """
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write(""" #my first app
          """)
+         
+         
+chart_data = pd.DataFrame(
+np.random.randn(20, 3),
+columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
