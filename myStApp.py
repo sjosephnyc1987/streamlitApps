e9fe180d841aa5c42bedf2e547585cdb4deb09ec
Stephen Joseph <66809337+sjosephnyc1987@users.noreply.github.com>
2022-05-13 14:47:05 -0500

    Add files via upload

---
+++ myStApp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 16:24:48 2022
+
+@author: stephenjoseph
+"""
+import streamlit as st
+import pandas as pd
+
+st.write(""" #my first app
+         """)
